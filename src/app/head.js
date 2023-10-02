a83db53f4c3918f85df5afee965aa86841a59d7d
PeiYun <ollylee8520@gmail.com>
2023-10-02 17:38:39 -0600

    add laoding/fixed favicon

--- src/app/head.js
+++ src/app/head.js
@@ -4,7 +4,7 @@ export default function Head() {
       <title>IMDb Clone</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="This is the IMDb clone website" />
-      <link rel="icon" href="./favicon.ico" />
+      <link rel="icon" href="/img/favicon.ico" sizes="any" />
     </>
   );
 }
